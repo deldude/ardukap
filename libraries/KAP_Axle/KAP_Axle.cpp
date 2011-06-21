651261026d7cf32dafb2cc1debbfb635079a1364
deldude@gmail.com <deldude@gmail.com@fbaba74f-6ff4-ea20-e719-4c0638e7e160>
2011-06-21 20:44:45 +0000

    modif de la licence

--- libraries/KAP_Axle/KAP_Axle.cpp
+++ libraries/KAP_Axle/KAP_Axle.cpp
@@ -1,9 +1,24 @@
 /*
- * KAP_Axle.cpp
- *
- *  Created on: 15 juin 2011
- *      Author: deldude
- */
+  arduKAP - June 2011
+  https://code.google.com/p/ardukap/
+  An Open Source Arduino based KAP Controler.
+ 
+  This program is free software: you can redistribute it and/or modify 
+  it under the terms of the GNU General Public License as published by 
+  the Free Software Foundation, either version 3 of the License, or 
+  (at your option) any later version. 
+
+  This program is distributed in the hope that it will be useful, 
+  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+  GNU General Public License for more details. 
+
+  You should have received a copy of the GNU General Public License 
+  along with this program. If not, see <http://www.gnu.org/licenses/>. 
+  
+  Thanks to the www.AeroQuad.com for providing such a nice code which
+  provide a lot of inspiration for the arduKAP controler code.
+*/
 
 #include "WProgram.h"
 #include "KAP_Axle.h"
